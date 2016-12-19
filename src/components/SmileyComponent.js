06808294f6707a9fcd235982163eea89e247cf22
Felipe Ryan <fryan@fryan-ltm22.internal.salesforce.com>
2016-12-19 12:48:05 +1100

    Now showing smileys

--- src/components/SmileyComponent.js
+++ src/components/SmileyComponent.js
@@ -1,11 +1,23 @@
 import React, { PropTypes } from 'react';
+import unsure from './img/unsure.png';
+import happy from './img/happy.png';
+import sad from './img/sad.png';
 
-const SmileyComponent = ({polarity, certainty}) => (
-    <div>
-        <p>Polairty: {polarity}</p>
-        <p>Certainty: {certainty}</p>
-    </div>
-)
+const SmileyComponent = ({polarity, certainty}) => {
+    
+        if(certainty <= 0.2 && certainty >= -0.2){
+            return <img src={unsure} />;
+        }
+        else if (polarity >=0 ) {
+            return <img src={happy} />
+        }
+        else if (polarity <=0 ) {
+            return <img src={sad} />
+        }
+        else {
+            return <img src='./img/neutral.png' />
+        }               
+}
 
 
 SmileyComponent.propTypes = {
